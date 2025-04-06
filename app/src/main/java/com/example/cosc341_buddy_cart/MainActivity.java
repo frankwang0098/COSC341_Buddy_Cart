b4c9db35180d8e23313d4f4296dbc39a6b513a42
Sarah160 <fikunayomiadelaja@gmail.com>
2025-04-05 19:30:54 -0700

    updated main page

--- app/src/main/java/com/example/cosc341_buddy_cart/MainActivity.java
+++ app/src/main/java/com/example/cosc341_buddy_cart/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.cosc341_buddy_cart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,10 +26,79 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView instructionText = findViewById(R.id.instructiontext);
+
+        Button priorityButton = findViewById(R.id.prioritybutton);
+        Button nowButton = findViewById(R.id.nowbutton);
+        Button scheduleButton = findViewById(R.id.schedulebutton);
+        Button orderButton = findViewById(R.id.orderbutton);
+
+        ImageButton paymentbutton = findViewById(R.id.paymentbutton);
+        ImageButton promoCodebutton = findViewById(R.id.promocodebutton);
+        ImageButton backbutton = findViewById(R.id.backbutton);
+        ImageButton instructionbutton = findViewById(R.id.instructionbutton);
+
+        Spinner itemDropdown = findViewById(R.id.itemdropdown);
+
+        backbutton.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "Going back to shopping", Toast.LENGTH_SHORT).show();
         });
+        orderButton.setOnClickListener(view -> {
+            Toast.makeText(this, "Order Sucessfully Placed", Toast.LENGTH_SHORT).show();
+            finish();
+        });
+        priorityButton.setOnClickListener(view -> {
+            Toast.makeText(this, "Priority Delivery Selected", Toast.LENGTH_SHORT).show();
+        });
+        nowButton.setOnClickListener(view -> {
+            Toast.makeText(this, "Regular Delivery Selected ", Toast.LENGTH_SHORT).show();
+        });
+        scheduleButton.setOnClickListener(view -> {
+            Toast.makeText(this, "Scheduled Delivery Selected", Toast.LENGTH_SHORT).show();
+        } );
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cartexample,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        itemDropdown.setAdapter(adapter);
+
+        itemDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position != 0){
+                    Toast.makeText(MainActivity.this, "Selected: "+parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        paymentbutton.setOnClickListener(view ->{
+            Intent intent = new Intent(MainActivity.this, SavedPaymentMethod.class);
+            startActivity(intent);
+        });
+        promoCodebutton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, PromoCodeActivity.class);
+            startActivity(intent);
+        });
+
+        instructionbutton.setOnClickListener(view -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+            final EditText input = new EditText(MainActivity.this);
+            builder.setView(input);
+            builder.setPositiveButton("OK",(dialog, which) -> {
+                String instruction = input.getText().toString();
+                if(!instruction.isEmpty()) {
+                    instructionText.setText("Instructions: " + instruction);
+                }
+            });
+            builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
+            builder.show();
+        });
+
     }
 }
