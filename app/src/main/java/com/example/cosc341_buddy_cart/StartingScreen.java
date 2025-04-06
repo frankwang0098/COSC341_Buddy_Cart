a4f418103a6a58fef40744a7a4763d47c38b4f1b
Frank Wang <frankwang0098@gmail.com>
2025-04-05 17:40:55 -0700

    Added code for Buyer and Shopper buttons

--- app/src/main/java/com/example/cosc341_buddy_cart/StartingScreen.java
+++ app/src/main/java/com/example/cosc341_buddy_cart/StartingScreen.java
@@ -1,6 +1,9 @@
 package com.example.cosc341_buddy_cart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class StartingScreen extends AppCompatActivity {
+
+    private Button buyerButton;
+    private Button shopperButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,28 @@ public class StartingScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        buyerButton = findViewById(R.id.buyerButton);
+        shopperButton = findViewById(R.id.shopperButton);
+
+        buyerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create intent to start MainActivity
+                Intent intent = new Intent(StartingScreen.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        shopperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create intent to start MainActivityShopper
+                //Intent intent = new Intent(StartingScreen.this, MainActivityShopper.class);
+                //startActivity(intent);
+            }
         });
     }
 }
