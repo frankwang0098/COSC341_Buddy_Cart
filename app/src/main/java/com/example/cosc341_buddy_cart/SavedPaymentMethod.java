e24572b489edb5ac5f60118f4c76641fe2ae38f9
Sarah160 <fikunayomiadelaja@gmail.com>
2025-04-05 19:55:22 -0700

    saved payment page added

--- app/src/main/java/com/example/cosc341_buddy_cart/SavedPaymentMethod.java
+++ app/src/main/java/com/example/cosc341_buddy_cart/SavedPaymentMethod.java
@@ -1,6 +1,11 @@
 package com.example.cosc341_buddy_cart;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SavedPaymentMethod extends AppCompatActivity {
-
+public class SavedPaymentMethod  extends AppCompatActivity {
+    private SharedPreferences sharedPreferences;
+    private TextView savedPaymentMethodText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_saved_payment_method);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageButton backbutton = findViewById(R.id.backbutton2);
+        ImageButton addPaymentButton = findViewById(R.id.addpaymentbutton);
+        savedPaymentMethodText = findViewById(R.id.Savedpaymentmethod);
+
+        sharedPreferences = getSharedPreferences("Paymentprefs", MODE_PRIVATE);
+        String savedPayment = sharedPreferences.getString("card_info", "No saved payment Methods");
+        savedPaymentMethodText.setText(savedPayment);
+
+        View.OnClickListener goBackListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        };
+        backbutton.setOnClickListener(goBackListener);
+
+        addPaymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (SavedPaymentMethod.this, PaymentActivity.class);
+                startActivity(intent);
+            }
         });
+    }
+    @Override
+    protected  void onResume( ) {
+        super.onResume();
+        String updatedPayment = sharedPreferences.getString("card_info", "No saved Payment methods");
+        savedPaymentMethodText.setText(updatedPayment);
     }
 }
